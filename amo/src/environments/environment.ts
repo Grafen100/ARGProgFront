792bd022b46c7207deb1b5fc1e1d09a382fc76b5
Hannover 1917 <99442821+Grafen100@users.noreply.github.com>
2023-06-22 21:02:23 -0300

    delete firebase
    delete firebase

--- amo/src/environments/environment.ts
+++ amo/src/environments/environment.ts
@@ -3,20 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'projectam0-app',
-    appId: '1:304185596943:web:e4c4382b85abe09db1577a',
-    storageBucket: 'projectam0-app.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyAg_GL_W2_Ga30snZChhuuZV4aoMViKxqI',
-    authDomain: 'projectam0-app.firebaseapp.com',
-    messagingSenderId: '304185596943',
-    measurementId: 'G-CEM47SH3SG',
-  },
+ 
  
   production: false,
-  /*DEVELOPMENT --> */  URL: 'http://localhost:8080/',
-  /*PRODUCTION --> */ //URL: 'https://backam0.onrender.com/'
+  /*DEVELOPMENT --> */  //URL: 'http://localhost:8080/',
+  /*PRODUCTION --> */ URL: 'https://backam0.onrender.com/'
 
 }
 
